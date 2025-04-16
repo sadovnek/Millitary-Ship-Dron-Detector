4417cc62437bce05555e33681f2c1cd3a93855ac
Alex <silentsadovnek@gmail.com>
2025-04-16 09:08:10 +0300

    Update new-final.py

--- new-final.py
+++ new-final.py
@@ -8,6 +8,7 @@ from slib import ShipDetector, ShipTracker
 frame_lock = Lock()
 current_frame = None
 tracker = None
+result = -1
 height = 0
 width = 0
 state = 0  # 0 - detection, 1 - tracking
@@ -16,7 +17,7 @@ state = 0  # 0 - detection, 1 - tracking
 detector = ShipDetector(
     TF_LITE_MODEL='/home/pi/mind/models/lite-model_efficientdet_lite0_detection_metadata_1.tflite',
     LABEL_MAP='/home/pi/mind/labelmap.txt',
-    THRESHOLD=0.25
+    THRESHOLD=0.15
 )
 
 def track():
@@ -29,12 +30,13 @@ def track():
             success, bbox = tracker.tracking(local_frame)
             if success:
                 x, y, w, h = [int(v) for v in bbox]
-                cv2.rectangle(local_frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                print("TRACKING")
+                cv2.rectangle(current_frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
         
         time.sleep(0.01)
 
 def detect():
-    global state, tracker, current_frame
+    global state, tracker, current_frame, result
     while True:
         with frame_lock:
             local_frame = current_frame.copy() if current_frame is not None else None
@@ -56,7 +58,7 @@ threading.Thread(target=detect, daemon=True).start()
 threading.Thread(target=track, daemon=True).start()
 
 # Инициализация видео
-video_path = "videos/mil-test.mp4"
+video_path = "videos/fregate-move.mp4"
 cap = cv2.VideoCapture(video_path)
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -72,6 +74,7 @@ while True:
 
     # Отображение кадра с обработкой
     display_frame = current_frame.copy()
+    print(result,state)
     cv2.imshow('Boat Detection', display_frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
